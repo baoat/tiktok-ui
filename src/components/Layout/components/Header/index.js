57641f0e79fdc22ff6a586fb4545844a9312aacf
baoat <baoat191201@gmail.com>
2023-07-05 10:49:13 +0700

    Tạo Header/Sidebar

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,35 @@
+import style from './Header.module.scss';
+import classNames from 'classnames/bind';
+import logo_tikiok from '~/imgs/download.png';
+
+const cx = classNames.bind(style);
 function Header() {
   return (
     <>
-      <h2>Header</h2>
+      <header className={cx('header')}>
+        <div className={cx('container-header')}>
+          <div className={cx('logo-tiktok')}>
+            <img className={cx('logo')} src={logo_tikiok} alt />
+          </div>
+          <div className={cx('search')}>
+            <form className={cx('form-search')}>
+              <input type="text" className={cx('search-input')} />
+              <button className={cx('search-btn')}>Search</button>
+            </form>
+          </div>
+          <div className={cx('acc')}>
+            <div className={cx('upload')}>
+              <button className={cx('btn-upload')}>Upload</button>
+            </div>
+            <div className={cx('login')}>
+              <button className={cx('btn-login')}>Login</button>
+            </div>
+            <div className={cx('cover')}>
+              <button className={cx('btn-cover')}>...</button>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
